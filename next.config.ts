852395f3e072e1a0e6454ed8b43204d6fb88bc27
Amirmohammad <amirmuhaammaadd@gmail.com>
2025-08-27 09:27:52 +0330

    next/bundle-analyzer

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,19 @@
 import type { NextConfig } from "next";
 
+// const withBundleAnalyzer = require("@next/bundle-analyzer")({
+//   enabled: process.env.ANALYZE === "true",
+// });
+
+// module.exports = withBundleAnalyzer({
+//   // سایر تنظیمات پروژه
+// });
+
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: false,
 };
 
-export default nextConfig;
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer(nextConfig);
